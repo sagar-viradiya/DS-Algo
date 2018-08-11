698ac37da927fdc671cd583a4914397f758c6a5e
Sagar <sagar.viradiya@bookmyshow.com>
2018-08-11 16:52:26 +0530

    Dynamic programming coin change problem

---
+++ DynamicProgramming/src/CoinChange.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    //Change this to test diff use cases
+    val input = intArrayOf(1, 2, 3)
+    val changeOf = 4
+
+    val combination = LongArray(changeOf + 1)
+    combination[0] = 1
+
+    for (coin in input) {
+        for (i in 1 until combination.size) {
+            if (i >= coin) {
+                combination[i] += combination[(i-coin)]
+            }
+        }
+    }
+
+    print(combination[changeOf])
+}
