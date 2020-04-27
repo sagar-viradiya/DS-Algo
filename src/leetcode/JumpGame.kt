286645c234cff295b1d28e3f71a090ea1f9058f2
Sagar Viradiya <sagarviradiya4444@gmail.com>
2020-04-27 21:31:14 +0530

    Jump game alternative approaches.

--- src/leetcode/JumpGame.kt
+++ src/leetcode/JumpGame.kt
@@ -21,10 +21,11 @@ package leetcode
                  jump length is 0, which makes it impossible to reach the last index.
  */
 fun main() {
-    val nums = intArrayOf(2, 7, 1, 1, 4)
+    val nums = intArrayOf(2, 2, 1, 0, 4)
     print(depthFirstSearch(0, nums, IntArray(nums.size) { if (it == nums.size - 1) 1 else -1 }))
 }
 
+//O(n*n) Top down DP
 private fun depthFirstSearch(start: Int, nums: IntArray, dp: IntArray): Boolean {
     if (dp[start] != -1) return dp[start] == 1
 
@@ -42,4 +43,32 @@ private fun depthFirstSearch(start: Int, nums: IntArray, dp: IntArray): Boolean 
 
     dp[start] = 0
     return false
+}
+
+//O(n*n) Bottom up DP
+private fun canJumpToLast(nums: IntArray): Boolean {
+    val dp = IntArray(nums.size) { if (it == nums.size - 1) 1 else -1 }
+    var furthestJump: Int
+    for (i in nums.size - 2 downTo 0) {
+        furthestJump = minOf(i + nums[i], nums.size - 1)
+        for (j in i+1..furthestJump) {
+            if (dp[j] == 1) {
+                dp[i] = 1
+                break
+            }
+        }
+    }
+
+    return dp[0] == 1
+}
+
+//Greedy approach O(n)
+private fun canJumpToLastGreedy(nums: IntArray): Boolean {
+    var lastGoodPosition = nums.size - 1
+    for (i in nums.size - 1 downTo 0) {
+        if (i + nums[i] >= lastGoodPosition) {
+            lastGoodPosition = i
+        }
+    }
+    return lastGoodPosition == 0
 }
